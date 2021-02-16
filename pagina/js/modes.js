10095a664a077fca6c6fe4ca70621f0787b02f8b
Mateo Rodriguez Fierro <mateo.rodriguez.ferro@gmail.com>
2021-02-16 17:13:09 -0500

    eventos añadidos, debuging faltante

--- pagina/js/modes.js
+++ pagina/js/modes.js
@@ -1,8 +1,39 @@
 //modo oscuro y calro
 
-function cambiarModo(){
+function cambiarModo()
+{
     var cuerpoweb = document.body;
     cuerpoweb.classList.toggle("oscuro");
 
+
+    document.querySelector("#modo").hidden = true;
+    document.querySelector("#modo").hidden = false;
+
+    var state = false;
+    var modoN = document.getElementById("modo");
+    modoN.innerHTML = "modo nocturno"
+    
+    modoN.onclick = function(){
+        state = !state;
+        if (state)
+        {
+            modoN.innerHTML = "modo nocturno";
+            
+        } else
+        {
+            modoN.innerHTML = "modo diurno";
+        }
+        cuerpoweb.classList.toggle("oscuro");
+        
+    }
+    
 }
+
+//eventos
+
+
+
+
+
+
 
