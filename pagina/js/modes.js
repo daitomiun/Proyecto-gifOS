c7554ac9c4d800872b1630ce42b4b7701d82eafe
Mateo Rodriguez Fierro <mateo.rodriguez.ferro@gmail.com>
2021-02-22 19:06:17 -0500

    funcionalidad en proceso termino en unos dias

--- pagina/js/modes.js
+++ pagina/js/modes.js
@@ -1,35 +1,14 @@
 //modo oscuro y calro
 
-function cambiarModo()
+var cuerpoweb = document.body;
+var modo = document.querySelector("#modo a");
+
+
+modo.onclick = function()
 {
-    var cuerpoweb = document.body;
     cuerpoweb.classList.toggle("oscuro");
-
-
-    document.querySelector("#modo").hidden = true;
-    document.querySelector("#modo").hidden = false;
-
-    var state = false;
-    var modoN = document.getElementById("modo");
-    modoN.innerHTML = "modo nocturno"
-    
-    modoN.onclick = function(){
-        state = !state;
-        if (state)
-        {
-            modoN.innerHTML = "modo nocturno";
-            
-        } else
-        {
-            modoN.innerHTML = "modo diurno";
-        }
-        cuerpoweb.classList.toggle("oscuro");
-        
-    }
-    
+    modo.innerHTML = cuerpoweb.classList.contains("oscuro") ? "modo diurno" : "modo nocturno";
 }
-
-//eventos
 
 
 
