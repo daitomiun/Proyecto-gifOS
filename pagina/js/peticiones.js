e83acb188950c7f84789243c13e76a787a18b911
Mateo Rodriguez Fierro <mateo.rodriguez.ferro@gmail.com>
2021-03-03 16:21:31 -0500

    maquetacion 75% completada, falta crear gifos

---
+++ pagina/js/peticiones.js
@@ -0,0 +1,17 @@
+/* https://developers.giphy.com/explorer/ */
+
+const fetchData = require("./fetchData.js");
+let apikey = "j2lEFfMiqpn38YVWrgdRb3YmJjuJUPnt";
+let limit = 2;
+const API = `https://api.giphy.com/v1/gifs/trending?api_key=${apikey}&limit=${limit}`;
+
+const prueba = async (url_api) => {
+  try {
+    const info = await fetchData(url_api);
+    console.log(info.pagination.total_count);
+  } catch (Error) {
+    console.error(Error);
+  }
+};
+
+prueba(API);
