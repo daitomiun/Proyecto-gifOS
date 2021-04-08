aa8bb406a41468579c7445201f932f133c8334ac
Mateo Rodriguez Fierro <mateo.rodriguez.ferro@gmail.com>
2021-04-08 09:43:25 -0500

    barra de busqueda casi terminada, falta debugin y las sugerencias

--- pagina/js/peticiones.js
+++ pagina/js/peticiones.js
@@ -76,60 +76,72 @@
         var uInput= document.getElementById("search").value;
         var apiKey = "VZ4N6ebz6BSdgrhUNiKAAU0dNYws5GSn";
         
+        let maxLimit = 48
         let limit = 12;
-        let container = document.getElementById("gif-1");
-        
-
-        
-        // const fetchGif = await fetch(`https://api.giphy.com/v1/gifs/search?q=${uInput}&api_key=${apiKey}&limit=12`)
-        
-
-        const fetchGif = await fetch(`https://api.giphy.com/v1/gifs/search?q=${uInput}&api_key=${apiKey}&limit=${limit}`)
+        const fetchGif = await fetch(`https://api.giphy.com/v1/gifs/search?q=${uInput}&api_key=${apiKey}&limit=${maxLimit}`)
         let dataGif= await fetchGif.json();
         
         
-        let recomend = document.getElementsByName("written-recomend")
+      
         
-        for (let i = 0; i<limit; i++){
-          
-                  console.log(dataGif);
-                  console.log(dataGif.data[0].images.fixed_height.url);
-                  let imgPath = dataGif.data[0].images.fixed_width.url;
-          
-                  let gif = document.createElement("img");
-                  gif.setAttribute("src", imgPath);
-                  container.appendChild(gif);
+        
+        
+        // const fetchGif = await fetch(`https://api.giphy.com/v1/gifs/search?q=${uInput}&api_key=${apiKey}&limit=12`)
+        
+        let recomend = document.getElementsByClassName("written-recomend");
+        
+        for (let i = 1; i<=limit; i++){
          
-         let element = document.createElement("div");
-         element.className = "gif-card";
+        let element = document.createElement("div");
+        
+        element.className = "gif-card";
   
-         element.innerHTML = 
-         `
-         <div id="gif-1"></div>
-         <div class="gif-box">
-           <div class="gif-icons">
-             <div class="icon-box heart">
-               <i class='fas fa-search-plus'></i>
-             </div>
-             <div class="icon-box download">
-               <i class="fas fa-download"></i>
-             </div>
-             <div class="icon-box zoom">
-               <i class="far fa-heart"></i>
-             </div>
-           </div>
-           <div class="adaptive-text">
-             <P class="gif-title">Nombre gifos</P>
-           </div>
-         </div>
-         `;
-         recomend.appendChild(element);
+        element.innerHTML = `
+        <div id="gif-${i}"></div>
+        <div class="gif-box" style="width: 260px; height: 200px;">
+            <div class="gif-icons">
+                <div class="icon-box heart">
+                    <i class='fas fa-search-plus'></i>
+                </div>
+                <div class="icon-box download">
+                    <i class="fas fa-download"></i>
+                </div>
+                <div class="icon-box zoom">
+                    <i class="far fa-heart"></i>
+                </div>
+            </div>
+            <div class="adaptive-text">
+                <P class="gif-title">Nombre gifos</P>
+            </div>
+        </div>
+        `;
+        element.style.width = "260px";
+        element.style.height = "200px";
+        recomend[0].appendChild(element);
+        
+        let container = document.getElementById(`gif-${i}`);
+        container.style.width = "260px";
+        container.style.height = "200px";
+        container.style.position = "absolute";
+        container.style.display = "block";
+
+
+        console.log(dataGif);
+        console.log(dataGif.data[i].images.fixed_height.url);
+        let imgPath = dataGif.data[i].images.fixed_width.url;
+        
+        let gif = document.createElement("img");
+        gif.setAttribute("src", imgPath);
+        gif.style.width = "260px";
+        gif.style.height = "200px";
+        gif.style.display = "block";
+        container.appendChild(gif);
+
        }
         
-        
       }
+      // let limit = 12;
       
-
 
       
       //gifCall();
