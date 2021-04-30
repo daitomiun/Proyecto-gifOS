29fddb405dbfa9f3f5643d47ab24b801b57a604a
Mateo Rodriguez Fierro <mateo.rodriguez.ferro@gmail.com>
2021-04-30 15:01:07 -0500

    falta poco para terminar toda bara de busqueda

--- pagina/js/peticiones.js
+++ pagina/js/peticiones.js
@@ -45,16 +45,16 @@
             console.log(insert);
             console.log(`User input ${counter}`);
             
-            if (counter>=2 ) {
+            deleteGifs();
+            // if (counter>=2 ) {
                 
-                counter = 0;
-                // console.log("son diferentes")
-                // delete gifCall();
-                return deleteGifs();
-                // return gifCall();
+            //     counter = 0;
+            //     console.log("son diferentes")
+            //     delete gifCall();
+            //     return gifCall();
                 
              
-            }
+            // }
 
     }
 
@@ -65,24 +65,21 @@
     function deleteGifs(){
 
         let requested = document.getElementById("show-requested")
+        let quitAllWrittenText = document.getElementById( "reflected-input")
         let recomend = document.getElementById("written-recomend");
-        recomend.parentNode.removeChild(recomend);
-        
-        let createElement= document.createElement("div");
+        //let quitAllMore = document.getElementById("more-button");
 
+        requested.parentNode.removeChild(quitAllWrittenText);
+        recomend.parentNode.removeChild(recomend);
+        //requested.parentNode.removeChild(quitAllMore)
+
+        let createElement = document.createElement("div")
         createElement.className = "written-recomend";
         createElement.id = "written-recomend";
-
-        let createMore = document.createElement("div");
-        createMore.className= "more-button";
-        createMore.innerHTML =`<p>buscar mas</p>`;
         
 
-       
-
-
         requested.appendChild(createElement)
-        requested.appendChild(createMore)
+        //requested.appendChild(createMore)
         
         // let deleteGif = document.querySelector("#delete");
         // for(let i= 12; i>0; i--){
@@ -105,18 +102,34 @@
       async function gifCall(){
           
         ++counter;
+        let requested = document.getElementById("show-requested")
         console.log(`gif call ${counter}`);
         let apiKey = "VZ4N6ebz6BSdgrhUNiKAAU0dNYws5GSn";
         let uInput= document.getElementById("search").value;
+        let tRecomend = document.getElementById("trend-recomend")
+        console.log(tRecomend)
+        let createReflected = document.createElement("div")
+        createReflected.className = "title-align"
+        createReflected.id = "reflected-input"
+        createReflected.innerHTML = `${uInput}`;
+        console.log("cambio:  "+createReflected);
+        tRecomend.insertBefore(createReflected, requested)   
         
+        // let reflected = document.getElementsById("reflected-input");
+
+        
+
+    
+               
         
         let maxLimit = 48
         let limit = 12;
         const fetchGif = await fetch(`https://api.giphy.com/v1/gifs/search?q=${uInput}&api_key=${apiKey}&limit=${maxLimit}`)
         let dataGif= await fetchGif.json();
-        
-        
+        console.log("inst working:  "+dataGif)
+        // let tRecomend = document.getElementsByClassName("trend-recomend")
         let recomend = document.getElementsByClassName("written-recomend");
+        // tRecomend.appendChild(createReflected)
         
         for (let i = 1; i<=limit; i++){
          
@@ -146,26 +159,28 @@
             </div>
             `;
         
-            if (counter>1 || uInput === "") {
+
+            // if (counter>1 || uInput === "") {
             
-            counter = 0;
-            delete element;
-            delete recomend;
+            // counter = 0;
+            // delete element;
+            // delete recomend;
             
-            break;
+            // break;
             
-            }
-            if (uInput == "" ) {
+            // }
+            // if (uInput == "" ) {
             
             
-            delete element;
-            delete recomend;
-            break;
+            // delete element;
+            // delete recomend;
+            // break;
             
-            }
+            // }
+            console.log(dataGif);
             element.style.width = "260px";
             element.style.height = "200px";
-            recomend[0].appendChild(element);
+            recomend[0].appendChild(element)[0];
             
             let container = document.getElementById(`gif-${i}`);
 
@@ -174,8 +189,8 @@
             container.style.position = "absolute";
             container.style.display = "block";
         
-            //console.log(dataGif);
-            //console.log(dataGif.data[i].images.fixed_height.url);
+            // console.log(dataGif);
+            console.log(dataGif.data[i].images.fixed_height.url);
             let imgPath = dataGif.data[i].images.fixed_width.url;
 
         
@@ -202,17 +217,37 @@
 
                 }, 3000);
        }
-      
+        let createMore = document.createElement("div");
+        createMore.className= "more-button";
+        createMore.id = "more-button";
+        createMore.innerHTML =`<p>buscar mas</p>`;
        
+        requested.appendChild(createMore)
     }
     
-    // async function recomendations(){
-    //     let uInput= document.getElementById("search").value;
+    async function recomendations(){
+        let apiKey = "VZ4N6ebz6BSdgrhUNiKAAU0dNYws5GSn";
+        let uInput= document.getElementById("search").value;
+        // let prueba = "hola"
         
-    //     const fetchSugest =await fetch(`api.giphy.com/v1/tags/related/{term}`);
-    //     laData =await  fetchSugest.json();
-    //     console.log(laData);
-    // }
+        if(uInput == null || uInput.length == 0){
+            console.log("no hay nada")
+            return false;
+        }else{
+            
+            console.log("se esta cambiando algo")
+            const fetchSugest =await fetch(`https://api.giphy.com/v1/gifs/search/tags?q=${uInput}?&api_key=${apiKey}`);
+            let laData =await  fetchSugest.json();
+    
+            // console.log(fetchSugest);
+            
+            console.log( laData);
+        }
+        
+    
+    }
+    recomendations();
+    
 
 
 
@@ -226,6 +261,7 @@
     var trendData = [];
     
     async function trending(){
+        
             let apiKey = "VZ4N6ebz6BSdgrhUNiKAAU0dNYws5GSn";
             const apiTrendfetch = await fetch(`https://api.giphy.com/v1/gifs/trending?&api_key=${apiKey}`)
             let dataGif = await apiTrendfetch.json();
@@ -271,8 +307,8 @@
                 container.style.position = "absolute";
                 container.style.display = "block";
               
-                //console.log(dataGif);
-                //console.log(dataGif.data[i].images.fixed_height.url);
+                console.log(dataGif);
+                console.log(dataGif.data[i].images.fixed_height.url);
                 let imgPath = dataGif.data[i].images.fixed_width.url;
 
         
@@ -343,7 +379,7 @@
       
 
 
-     
+   
       
 
 
